bddac9d9b953b9776c075bfc1719d1185cf185ff
Jakob Voss <voss@gbv.de>
2018-06-28 15:16:22 +0200

    Cleanup EntityMapper

--- test/exports.js
+++ test/exports.js
@@ -1,15 +1,34 @@
 const should = require("should")
-const { mappingTypes, getMappingSchemes } = require("../lib/wikidata-wrapper")
+const wds = require("../lib/wikidata-wrapper")
 
 describe("exported constants and functions", () => {
 
+  [
+    "identifiers",
+    "labels",
+    "aliases",
+    "descriptions",
+    "claims",
+    "sitelinks",
+    "info",
+    "entity"
+  ].forEach(func => {
+    it(`map.${func}`, () => {
+      should(wds.map[func]).be.Function()
+    })
+  })
+
+  it("entityTypes", () => {
+    should(wds.entityTypes).be.Object()
+  })
+
   it("mappingTypes", () => {
-    should(mappingTypes).be.Object()
-    should(mappingTypes.Q39893184).equal("http://www.w3.org/2004/02/skos/core#closeMatch")
+    should(wds.mappingTypes).be.Object()
+    should(wds.mappingTypes.Q39893184).equal("http://www.w3.org/2004/02/skos/core#closeMatch")
   })
 
   it("getMappingSchemes", () => {
-    should(getMappingSchemes).be.Function()
+    should(wds.getMappingSchemes).be.Function()
   })
 
 })
