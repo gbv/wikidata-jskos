6765d341008e5513a06a653539dbc7687d2f968d
Stefan Peters <Stefan.Peters@gbv.de>
2020-10-07 12:38:49 +0200

    Fix issue for /mappings when fromScheme or toScheme are not known (#63)

--- test/mappings.js
+++ test/mappings.js
@@ -27,4 +27,25 @@ describe("getMappings", () => {
         mappings.should.deep.equal([require("./Q42.gnd.json")])
       })
   })
+
+  it("returns no mappings for unknown fromScheme", () => {
+    const fromScheme = "unknown:scheme"
+    const toScheme = "http://bartoc.org/en/node/430"
+
+    return service.getMappings({ fromScheme, toScheme })
+      .then(mappings => {
+        mappings.should.deep.equal([])
+      })
+  })
+
+  it("returns no mappings for unknown toScheme", () => {
+    const from = "http://www.wikidata.org/entity/Q42"
+    const toScheme = "unknown:scheme"
+
+    return service.getMappings({ from, toScheme })
+      .then(mappings => {
+        mappings.should.deep.equal([])
+      })
+  })
+
 })
